a63f3c0b37b6b935e26bf16133734b3fa0019c15
Ata-92 <atakal92@gmail.com>
2021-05-13 16:04:54 +0300

    Add getData function, useState and useEffect

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import location from "../src/assets/location.svg";
 import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  const getData = () => {
+    axios
+      .get("https://randomuser.me/api/")
+      .then((res) =>
+        res.data.results.map((user) => ({
+          image: `${user.picture.large}`,
+          name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+          email: `${user.email}`,
+          phone: `${user.cell}`,
+          location: `${user.location.city} - ${user.location.country}`,
+          age: `Age: ${user.dob.age}`,
+          registration: `Register Date: ${user.registered.date.slice(0, 10)}`,
+        }))
+      )
+      .then((data) => setUser(data));
+  };
+
+  useEffect(getData, []);
+
   return (
     <div className="App">
     </div>
