58e1e2222f10ec97b1583279e55e37a84933a38a
Ata-92 <atakal92@gmail.com>
2021-05-13 16:03:21 +0300

    Import axios and images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import email from "../src/assets/email.svg";
+import phone from "../src/assets/phone.svg";
+import location from "../src/assets/location.svg";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
